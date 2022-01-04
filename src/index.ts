d0839ab503f6ee82fbb60371a95dcafa7b255901
jinzhan <jinzhan@baidu.com>
2022-01-04 17:36:57 +0800

    add .prettierrc

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+/**
+ * @file san test utils main file
+ **/
+
+export {default as mount} from './attach';
+export {default as shallowMount} from './shallowAttach';
+export {default as config} from './config';
+export {createWrapper as createWrapper} from './wrapper/wrapper';
---
+++ src/shallowAttach.ts
@@ -0,0 +1,16 @@
+/**
+ * @file san test utils shallow mount file
+ **/
+
+import attach from './attach';
+import { ComponentWithPrototype, LooseObject, MergedComponentOptions } from '../types';
+import {getNewComponent} from './utils/createComponent';
+import {createAllStubComponents} from './utils/createComponentStubs';
+
+export default function (component: ComponentWithPrototype, options: MergedComponentOptions | LooseObject = {}) {
+    const stubbedComponents = createAllStubComponents(component);
+    const newComponent = getNewComponent(component);
+
+    newComponent.components = stubbedComponents;
+    return attach(newComponent, options as MergedComponentOptions);
+}
